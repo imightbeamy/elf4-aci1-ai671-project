35b0bc28084d060e81f39972acf0a3e7ce225ca6
amyciav <amyciav@d7cea4cd-4c98-4bee-9144-f8bbac24aada>
2010-11-29 00:49:05 +0000

    Added probability functions to BayesSolver

--- src/aiProj/BayesSolver.java
+++ src/aiProj/BayesSolver.java
@@ -12,4 +12,25 @@ public class BayesSolver extends Solver{
 		
 	}
 
+	double Prob_ui_given_evidence(int i, int correct_j, int incorrect_k)
+	{
+		return Prob_evidence_given_ui(i,correct_j,incorrect_k)/Prob_evidence(correct_j, incorrect_k);
+	}
+	
+	double Prob_evidence(int correct_j, int incorrect_k)
+	{
+		double p = 0;
+		for(int i = 0; i < numConcepts - 1; i++)
+		{
+			float a = i / (numConcepts - 1);
+			p+=Math.pow(a,correct_j) * Math.pow(1 - a,incorrect_k);
+		}
+		return 0;
+	}
+	
+	double Prob_evidence_given_ui(int i, int correct_j, int incorrect_k)
+	{
+		float a = i / (numConcepts - 1);
+		return  Math.pow(a,correct_j) * Math.pow(1 - a,incorrect_k);
+	}
 }
