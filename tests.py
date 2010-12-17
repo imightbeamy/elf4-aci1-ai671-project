377999b8a03ca2e19e433d3ed9f22e4d107bd394
amyciav <amyciav@d7cea4cd-4c98-4bee-9144-f8bbac24aada>
2010-12-17 05:54:59 +0000

    fixed assignment statement error. Yes. That was the problem. For realz.

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+
 import os
 import time
 
@@ -5,9 +6,9 @@ problems = [10, 50, 100, 500]
 concepts = [2, 3, 4, 5, 6, 7, 8]
 ulevels = 4
 maxProblemConcepts = 2
-runs = 100
+runs = 10
 path = "./bin"
-os.system("rm -rf summery.csv")
+os.system("rm -rf summa  ry.csv")
 
 for r in range(runs):
 	for p in problems:
@@ -20,4 +21,6 @@ for r in range(runs):
 					str(maxProblemConcepts)
 			os.system("java -Xms32m -Xmx600m -cp " + path + " aiProj/FinalProject "\
 						+ args + " > " + filename)
-	print "Test " + str(r) + " finished" 
+	print "Test " + str(r) + " finished"
+	
+	
