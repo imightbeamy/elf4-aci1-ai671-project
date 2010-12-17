c9e0b8f61e79d4475d412df4d3f4d412d237be00
amyciav <amyciav@d7cea4cd-4c98-4bee-9144-f8bbac24aada>
2010-12-17 16:59:35 +0000

    formatted

--- src/aiProj/StudentConceptFramework.java
+++ src/aiProj/StudentConceptFramework.java
@@ -2,95 +2,84 @@ package aiProj;
 
 import java.util.ArrayList;
 
-
-
-public class StudentConceptFramework{
+public class StudentConceptFramework {
 
 	private ArrayList<StudentConcept> concepts;
-	private int levels; 
+	private int levels;
 	private int numConcepts;
-	
+
 	public StudentConceptFramework(int levels, int numConcepts) {
 		initialize(levels, numConcepts);
 	}
-	
-	private void initialize(int levels, int numConcepts){
+
+	private void initialize(int levels, int numConcepts) {
 		concepts = new ArrayList<StudentConcept>(numConcepts);
-		for (int i = 0; i < numConcepts; i++){
+		for (int i = 0; i < numConcepts; i++) {
 			concepts.add(null);
 		}
 		this.levels = levels;
 		this.numConcepts = numConcepts;
-		
+
 	}
 
 	public StudentConceptFramework(int numConcepts) {
-		this(1,numConcepts);
-	}
-	
-	public StudentConceptFramework(ArrayList<Integer> setting,
-			int levels) {
-		  initialize(levels, setting.size());
-		  int conceptIdx = 0;
-		  for (Integer i : setting){
-			  setUnderstanding(conceptIdx, i/(levels - 1.0));
-			  conceptIdx ++;
-		  }
-		  
+		this(1, numConcepts);
 	}
 
-	public void add(StudentConcept c){
+	public StudentConceptFramework(ArrayList<Integer> setting, int levels) {
+		initialize(levels, setting.size());
+		int conceptIdx = 0;
+		for (Integer i : setting) {
+			setUnderstanding(conceptIdx, i / (levels - 1.0));
+			conceptIdx++;
+		}
+
+	}
+
+	public void add(StudentConcept c) {
 		concepts.ensureCapacity(numConcepts);
 		int id = c.getId();
-		if(id < concepts.size())
-		{
-			concepts.set(id,c);
+		if (id < concepts.size()) {
+			concepts.set(id, c);
 		}
 	}
 
-
-	public double getAbility(Concept c) {		
+	public double getAbility(Concept c) {
 		int id = c.getId();
 		return concepts.get(id).getLevel();
 	}
-	
-	public void setUnderstanding(StudentConcept c, int level)
-	{
-		setUnderstanding(c.getId(), level/(levels - 1.0));
+
+	public void setUnderstanding(StudentConcept c, int level) {
+		setUnderstanding(c.getId(), level / (levels - 1.0));
 	}
 
-	public void setUnderstanding(int studentConceptID, double level)
-	{
-		if(studentConceptID < concepts.size() &&
-				concepts.get(studentConceptID) != null)
-		{
-			concepts.get(studentConceptID).setLevel((float)level);
-		}
-		else {
+	public void setUnderstanding(int studentConceptID, double level) {
+		if (studentConceptID < concepts.size()
+				&& concepts.get(studentConceptID) != null) {
+			concepts.get(studentConceptID).setLevel((float) level);
+		} else {
 			add(new StudentConcept(studentConceptID, (float) level));
 		}
 	}
-	
-		
-	public int numberofConcepts()
-	{
+
+	public int numberofConcepts() {
 		return concepts.size();
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String toReturn = "";
-		for (StudentConcept c: concepts){
+		for (StudentConcept c : concepts) {
 			toReturn += c.toString();
 			toReturn += '\n';
 		}
-		
-		
+
 		return toReturn;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public StudentConceptFramework clone(){
-		StudentConceptFramework clone = new StudentConceptFramework(levels, numConcepts);
+	public StudentConceptFramework clone() {
+		StudentConceptFramework clone = new StudentConceptFramework(levels,
+				numConcepts);
 		clone.concepts = (ArrayList<StudentConcept>) concepts.clone();
 		return clone;
 	}
